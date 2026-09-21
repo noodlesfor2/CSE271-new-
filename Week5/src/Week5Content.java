dd6a0c11e0abb8136508b61c64a92cec22e4d1cc
noodlesfor2 <jonahmoore055@gmail.com>
2026-09-21 00:04:36 -0400

    New commit

---
+++ Week5/src/Week5Content.java
@@ -0,0 +1,21 @@
+import java.awt.Point;
+import java.util.Scanner;
+
+public class Week5Content {
+    public static void main(String[] args) {
+        
+        // ***** OBJECTS *****
+        
+        // Object I already know and use: Scanner
+        
+        Scanner kb = new Scanner (System.in);
+        
+        // Point object: does this work?
+        Point p = new Point(13, 14);
+        
+        System.out.println(p.toString());
+        
+        
+        
+    }
+}
